da7c09eca1fcdc167c8e7e2e07ba00127475611c
Elisabeth Melaku <4138@htl.rennweg.at>
2018-01-26 13:20:24 +0100

    Aufgabe 3 erstellt: AJAX wird verwendet, flags.php wird vom Server geladen.

--- index.php
+++
@@ -1,9 +0,0 @@
-<!--brauchen wir nicht mehr-->
-<?php
-//namespace HTL3R\MelakuYazykova\Superflagbowl;
-//
-//require "./vendor/autoload.php";
-//use HTL3R\Flags\Flags as Flag;
-//
-//require "flags.php";
-
