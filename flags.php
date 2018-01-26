172803498f93ef6b548c769ef096abe28ef9e4b3
Elisabeth Melaku <4138@htl.rennweg.at>
2018-01-26 12:54:41 +0100

    Aufgabe 2 erstellt: Erweitern der Datei mit JSON. Mitsenden der korrekten Datei Header.

---
+++ flags.php
@@ -0,0 +1,54 @@
+<?php
+
+require "./vendor/autoload.php";
+use HTL3R\Flags\Flags as Flag;
+
+$flage1 = new Flag\TriangleFlag(
+    "Irl",
+    20,
+    10,
+    29,
+    "red"
+);
+
+$flage2 = new Flag\TriangleFlag(
+    "AUT",
+    20,
+    10,
+    29,
+    "blue"
+);
+
+$flage3 = new Flag\TriangleFlag(
+    "FR",
+    20,
+    10,
+    29,
+    "green"
+);
+//
+//echo $flage1;
+//echo $flage2;
+//echo $flage3;
+
+$a = array(
+    Objektumwandeln($flage1),
+    Objektumwandeln($flage2),
+    Objektumwandeln($flage3)
+);
+
+header('Content-Type: application/json');
+
+echo json_encode($a);
+
+function Objektumwandeln($flag ){
+    $obj = [];
+    $obj["name"] = $flag->getName();
+    $obj["preis"] = $flag->getPrice();
+    $obj["breite"] = $flag->getWidth();
+    $obj["hoehe"] = $flag->getHeight();
+    $obj["farbe"] = $flag->getColor();
+
+    return $obj;
+}
+
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
+<!--brauchen wir nicht mehr-->
 <?php
-namespace HTL3R\MelakuYazykova\Superflagbowl;
-
-require "./vendor/autoload.php";
-use HTL3R\Flags\Flags as Flag;
-
-require "flags.php";
+//namespace HTL3R\MelakuYazykova\Superflagbowl;
+//
+//require "./vendor/autoload.php";
+//use HTL3R\Flags\Flags as Flag;
+//
+//require "flags.php";
 
